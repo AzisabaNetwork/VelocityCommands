6cb8dd069b6641463982774d5d84fe38b84fe53b
acrylic-style <me@acrylicstyle.xyz>
2022-03-26 13:51:23 +0900

    now uses VelocityRedisBridge for all /send commands

--- src/main/java/net/azisaba/velocityCommands/commands/CommandSend.java
+++ src/main/java/net/azisaba/velocityCommands/commands/CommandSend.java
@@ -3,24 +3,13 @@ package net.azisaba.velocityCommands.commands;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.velocitypowered.api.command.CommandSource;
-import com.velocitypowered.api.proxy.ConnectionRequestBuilder;
 import com.velocitypowered.api.proxy.Player;
-import com.velocitypowered.api.proxy.ServerConnection;
-import com.velocitypowered.api.proxy.server.RegisteredServer;
-import net.azisaba.velocityCommands.VelocityCommands;
-import net.azisaba.velocityCommands.util.Util;
 import net.azisaba.velocityredisbridge.VelocityRedisBridge;
 import net.azisaba.velocityredisbridge.util.PlayerInfo;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
-import net.kyori.adventure.text.format.TextDecoration;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.CompletableFuture;
-import java.util.stream.Collectors;
 
 public class CommandSend extends AbstractCommand {
     @Override
@@ -100,101 +89,48 @@ public class CommandSend extends AbstractCommand {
                 );
     }
 
-    private static void asyncConnectAndShowResults(CommandSource source, Collection<Player> players, RegisteredServer server) {
-        source.sendMessage(Component.text("Attempting to send " + players.size() + " players to " + server.getServerInfo().getName()).color(NamedTextColor.GREEN));
-        new Thread(() -> {
-            List<ConnectionRequestBuilder.Status> results =
-                    players.stream()
-                            .map(p -> p.createConnectionRequest(server))
-                            .map(ConnectionRequestBuilder::connect)
-                            .map(CompletableFuture::join)
-                            .map(ConnectionRequestBuilder.Result::getStatus)
-                            .collect(Collectors.toList());
-            showResults(source, results);
-        }).start();
-    }
-
-    private static void showResults(CommandSource source, List<ConnectionRequestBuilder.Status> results) {
-        int success = Util.count(results, ConnectionRequestBuilder.Status.SUCCESS);
-        int alreadyConnecting = Util.count(results, ConnectionRequestBuilder.Status.CONNECTION_IN_PROGRESS);
-        int cancelled = Util.count(results, ConnectionRequestBuilder.Status.CONNECTION_CANCELLED);
-        int failed = Util.count(results, ConnectionRequestBuilder.Status.SERVER_DISCONNECTED);
-        int alreadyConnected = Util.count(results, ConnectionRequestBuilder.Status.ALREADY_CONNECTED);
-        source.sendMessage(Component.text("Send Results:").color(NamedTextColor.GREEN).decorate(TextDecoration.BOLD));
-        source.sendMessage(Component.text("Success: " + success).color(NamedTextColor.GREEN));
-        source.sendMessage(Component.text("Already connecting: " + alreadyConnecting).color(NamedTextColor.GREEN));
-        source.sendMessage(Component.text("Cancelled: " + cancelled).color(NamedTextColor.GREEN));
-        source.sendMessage(Component.text("Failed: " + failed).color(NamedTextColor.GREEN));
-        source.sendMessage(Component.text("Already connected: " + alreadyConnected).color(NamedTextColor.GREEN));
-    }
-
     private static int sendEveryoneToPlayer(CommandSource source, String playerName) {
-        Optional<RegisteredServer> optServer = VelocityCommands.getProxy()
-                .getPlayer(playerName)
-                .flatMap(Player::getCurrentServer)
-                .map(ServerConnection::getServer);
-        if (!optServer.isPresent()) {
+        String toServer = VelocityRedisBridge.getApi().getPlayerInfo(playerName).map(PlayerInfo::getChildServer).orElse(null);
+        if (toServer == null) {
             return sendMessageMissingPlayer(source, playerName);
         }
-        RegisteredServer server = optServer.get();
-        Collection<Player> players = VelocityCommands.getProxy().getAllPlayers();
-        asyncConnectAndShowResults(source, players, server);
-        return players.size();
+        long count = VelocityRedisBridge.getApi().getAllPlayerInfo()
+                .stream()
+                .peek(p -> VelocityRedisBridge.getApi().sendPlayer(p.getUsername(), toServer))
+                .count();
+        source.sendMessage(Component.text("Attempting to send " + count + " players to " + toServer).color(NamedTextColor.GREEN));
+        return (int) count;
     }
 
     private static int sendEveryoneToServer(CommandSource source, String serverName) {
-        Optional<RegisteredServer> optServer = VelocityCommands.getProxy().getServer(serverName);
-        if (!optServer.isPresent()) {
-            return sendMessageMissingServer(source, serverName);
-        }
-        RegisteredServer server = optServer.get();
-        Collection<Player> players = VelocityCommands.getProxy().getAllPlayers();
-        asyncConnectAndShowResults(source, players, server);
-        return players.size();
+        long count = VelocityRedisBridge.getApi()
+                .getAllPlayerInfo()
+                .stream()
+                .peek(p -> VelocityRedisBridge.getApi().sendPlayer(p.getUsername(), serverName))
+                .count();
+        source.sendMessage(Component.text("Attempting to send " + count + " players to " + serverName).color(NamedTextColor.GREEN));
+        return (int) count;
     }
 
     private static int sendCurrentToPlayer(CommandSource source, String playerName) {
         Player player = (Player) source;
-        RegisteredServer currentServer = player.getCurrentServer().orElseThrow(IllegalStateException::new).getServer();
-        Optional<RegisteredServer> optServer = VelocityCommands.getProxy()
-                .getPlayer(playerName)
-                .flatMap(Player::getCurrentServer)
-                .map(ServerConnection::getServer);
-        if (!optServer.isPresent()) {
-            return sendMessageMissingPlayer(source, playerName);
-        }
-        RegisteredServer toServer = optServer.get();
-        Collection<Player> players = currentServer.getPlayersConnected();
-        asyncConnectAndShowResults(source, players, toServer);
-        return players.size();
+        String currentServer = player.getCurrentServer().orElseThrow(IllegalStateException::new).getServer().getServerInfo().getName();
+        return sendServerToPlayer(source, currentServer, playerName);
     }
 
     private static int sendCurrentToServer(CommandSource source, String serverName) {
         Player player = (Player) source;
-        RegisteredServer currentServer = player.getCurrentServer().orElseThrow(IllegalStateException::new).getServer();
-        Optional<RegisteredServer> optServer = VelocityCommands.getProxy().getServer(serverName);
-        if (!optServer.isPresent()) {
-            return sendMessageMissingServer(source, serverName);
-        }
-        RegisteredServer toServer = optServer.get();
-        Collection<Player> players = currentServer.getPlayersConnected();
-        asyncConnectAndShowResults(source, players, toServer);
-        return players.size();
+        String fromServer = player.getCurrentServer().orElseThrow(IllegalStateException::new).getServer().getServerInfo().getName();
+        return sendServerToServer(source, fromServer, serverName);
     }
 
     private static int sendPlayerToPlayer(CommandSource source, String fromPlayer, String toPlayer) {
-        Optional<Player> player = VelocityCommands.getProxy().getPlayer(fromPlayer);
-        if (!player.isPresent()) {
-            return sendMessageMissingPlayer(source, fromPlayer);
-        }
-        Optional<RegisteredServer> optServer = VelocityCommands.getProxy()
-                .getPlayer(toPlayer)
-                .flatMap(Player::getCurrentServer)
-                .map(ServerConnection::getServer);
-        if (!optServer.isPresent()) {
+        String toServer = VelocityRedisBridge.getApi().getPlayerInfo(toPlayer).map(PlayerInfo::getChildServer).orElse(null);
+        if (toServer == null) {
             return sendMessageMissingPlayer(source, toPlayer);
         }
-        asyncConnectAndShowResults(source, Collections.singletonList(player.get()), optServer.get());
+        VelocityRedisBridge.getApi().sendPlayer(fromPlayer, toServer);
+        source.sendMessage(Component.text("Attempting to send " + fromPlayer + " to " + toServer).color(NamedTextColor.GREEN));
         return 1;
     }
 
@@ -205,36 +141,23 @@ public class CommandSend extends AbstractCommand {
         }
         source.sendMessage(Component.text("Attempting to send " + player.get().getUsername() + " to " + toServer).color(NamedTextColor.GREEN));
         VelocityRedisBridge.getApi().sendPlayer(player.get().getUsername(), toServer);
-        //asyncConnectAndShowResults(source, Collections.singletonList(player.get()), optServer.get());
         return 1;
     }
 
     private static int sendServerToPlayer(CommandSource source, String fromServerName, String toPlayer) {
-        Optional<RegisteredServer> fromServer = VelocityCommands.getProxy().getServer(fromServerName);
-        if (!fromServer.isPresent()) {
-            return sendMessageMissingServer(source, fromServerName);
-        }
-        Optional<RegisteredServer> optServer = VelocityCommands.getProxy()
-                .getPlayer(toPlayer)
-                .flatMap(Player::getCurrentServer)
-                .map(ServerConnection::getServer);
-        if (!optServer.isPresent()) {
+        String toServer = VelocityRedisBridge.getApi().getPlayerInfo(toPlayer).map(PlayerInfo::getChildServer).orElse(null);
+        if (toServer == null) {
             return sendMessageMissingPlayer(source, toPlayer);
         }
-        asyncConnectAndShowResults(source, fromServer.get().getPlayersConnected(), optServer.get());
-        return 1;
+        return sendServerToServer(source, fromServerName, toPlayer);
     }
 
     private static int sendServerToServer(CommandSource source, String fromServerName, String toServer) {
-        Optional<RegisteredServer> fromServer = VelocityCommands.getProxy().getServer(fromServerName);
-        if (!fromServer.isPresent()) {
-            return sendMessageMissingServer(source, fromServerName);
-        }
-        Optional<RegisteredServer> optServer = VelocityCommands.getProxy().getServer(toServer);
-        if (!optServer.isPresent()) {
-            return sendMessageMissingServer(source, toServer);
-        }
-        asyncConnectAndShowResults(source, fromServer.get().getPlayersConnected(), optServer.get());
-        return 1;
+        long count = VelocityRedisBridge.getApi().getAllPlayerInfo().stream()
+                .filter(p -> p.getChildServer().equalsIgnoreCase(fromServerName))
+                .peek(pi -> VelocityRedisBridge.getApi().sendPlayer(pi.getUsername(), toServer))
+                .count();
+        source.sendMessage(Component.text("Attempting to send " + count + " players to " + toServer).color(NamedTextColor.GREEN));
+        return (int) count;
     }
 }
